eb230b6c492aaf1158559228c2e68b3f641f7518
Seohyun Jung <sxj393@case.edu>
2020-11-16 07:13:37 -0500

    adjusting some test values

--- run_tests.py
+++ run_tests.py
@@ -18,59 +18,47 @@ if __name__ == '__main__':
 	print("Running Test 1")
 
 	init_num_walks = int(m.log(100))
-	x_axis = [10, 50, 100, 150, 200]
+	x_axis = [100, 150, 200, 300, 400, 500]
 	y_axis = []
+	graph = get_graph(word, 100)
+
 	
 	for round in x_axis:
-		graph = get_graph(word, 100)
-		send_pipes, recv_pipes = get_graph_in_pipes(graph)
 		init_time = time()
-		pagerank(round, init_num_walks, graph, send_pipes, recv_pipes, reset_probability)
+		pagerank(round, init_num_walks, graph, reset_probability)
 		y_axis.append(time() - init_time)
 		
-		#finishing up connections for next test
-		for node in send_pipes:
-			for neighbor in send_pipes[node]:
-				neighbor[1].close()
-		for node in recv_pipes:
-			for neighbor in recv_pipes[node]:
-				neighbor[1].close()
+	for i in range(len(y_axis)):
+		y_axis[i] *= 1e3
 
 	plt.xlabel("number of rounds")
-	plt.ylabel("time in seconds")
+	plt.ylabel("time in milliseconds")
 	plt.plot(x_axis, y_axis)
-	plt.axis([0, 200, 0, (int(y_axis[4] / 10) + 1) * 10])
+	plt.axis([0, 500, 0, y_axis[4]])
 	plt.savefig('test1.png')
 
 	#Test 2, number of nodes v.s. time
 	#Fix number of rounds to 100
 
 	print("Running Test 2")
-	x_axis = [10, 50, 100, 150, 200]
+	x_axis = [100, 150, 200, 300, 500]
 	y_axis = []
 	num_rounds = 100
 
 	for num_nodes in x_axis:
 		graph = get_graph(word, num_nodes)
-		send_pipes, recv_pipes = get_graph_in_pipes(graph)
 		init_num_walks = int(m.log(num_nodes))
 		init_time = time()
-		pagerank(num_rounds, init_num_walks, graph, send_pipes, recv_pipes, reset_probability)
+		pagerank(num_rounds, init_num_walks, graph, reset_probability)
 		y_axis.append(time() - init_time)
 
-		#finishing up connections for next test
-		for node in send_pipes:
-			for neighbor in send_pipes[node]:
-				neighbor[1].close()
-		for node in recv_pipes:
-			for neighbor in recv_pipes[node]:
-				neighbor[1].close()
-
+	for i in range(len(y_axis)):
+		y_axis[i] *= 1e3
 
 	plt.xlabel("number of nodes")
-	plt.ylabel("time in seconds")
+	plt.ylabel("time in miliseconds")
 	plt.plot(x_axis, y_axis)
-	plt.axis([0, 200, 0, (int(y_axis[4] / 10) + 1) * 10])
+	plt.axis([0, 200, 0, y_axis[4] * 1e3])
 	plt.savefig('test2.png')
 
 	print("Tests done")
