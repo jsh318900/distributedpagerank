06bdd4cca28eec7c2c2ac3da46aaab0cc8faf261
Mei Wong <d881017+meiwong326@users.noreply.github.com>
2020-11-16 00:20:55 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,90 +1,62 @@
 import tkinter as tk
-import requests
+from get_links import get_graph, get_graph_in_pipes
+from page_rank import pagerank
+from functools import partial
 
 # Function to check the GUI works by printing out the graph
-def print_graph():
-    title = titleEntry.get()
-    numNodes = numNodesEntry.get()
-    graph = get_graph(title, int(numNodes))
-    text.config(state=tk.NORMAL)
-    text.delete(1.0, tk.END)
-    text.insert(tk.END, graph)
-    text.config(state=tk.DISABLED)
+class GUI:
+    def __init__(self, master):
+        # Create the main window
+        self.master = master
+        master.title("Distributed PageRank")
+        master.columnconfigure([0, 1, 2], minsize=150)
+        master.rowconfigure([0, 1, 2, 3], minsize=50)
+        
+        # Create a label asking for the title
+        self.titleLabel = tk.Label(text="Enter the title of the page: ")
+        self.titleLabel.grid(row=0, column=0, padx=15)
+         
+        self.titleEntry = tk.Entry()
+        self.titleEntry.grid(row=0, column=1, sticky='WE', padx=15, columnspan=2)
+        
+        # Create a label asking for the number of nodes
+        self.numNodesLabel = tk.Label(text="Enter the number of nodes: ")
+        self.numNodesLabel.grid(row=1, column=0, padx=15)
+        
+        self.numNodesEntry = tk.Entry()
+        self.numNodesEntry.grid(row=1, column=1, sticky='WE', padx=15, columnspan=2)
+        
+        # Create a text box to display the output
+        #self.textframe = tk.Frame(master)
+        #self.textframe.grid(row=3, column=0, sticky="WE", columnspan=3, rowspan=1)
+        self.text = tk.Text(height=2, width=50)
+        self.text.grid(row=3, column=0, sticky="WE", columnspan=3, rowspan=1, padx=15)
+        self.text.insert(tk.INSERT, 'Result will show up here')
+        
+        # Create a button to start the program
+        self.button = tk.Button(text="Run PageRank", command=self.print_graph)
+        self.button.grid(row=2, column=1)
     
+        
+    def print_graph(self):
+        title = self.titleEntry.get()
+        numNodes = self.numNodesEntry.get()
+        start_num_walks = 100   #TODO: this number should be changed to K, which is c*log(number_nodes)
+        num_rounds = 50         #TODO: this number should be changed to B*log(number_nodes/reset_probability)
+        reset_probability = 0.1
+        graph = get_graph(title, int(numNodes))
+        send_pipes, recv_pipes = get_graph_in_pipes(graph)
+        max_node = pagerank(num_rounds, start_num_walks, graph, send_pipes, recv_pipes, reset_probability)
+        self.text.config(state=tk.NORMAL)
+        self.text.delete(1.0, tk.END)
+        self.text.insert(tk.END, max_node)
+        self.text.config(state=tk.DISABLED)
+
+
+def main():
+    window = tk.Tk()
+    GUI(window)
+    window.mainloop()
     
-def get_graph(word, node_number):
-    S = requests.Session()
-    graph = {}
-    URL = "https://en.wikipedia.org/w/api.php"
-    relate=set()
-    relate.add(word)
-
-    count=0
-    while(len(graph)<node_number):
-        if len(relate)>0:
-            w=relate.pop()
-
-        else:
-            return graph
-        if w not in graph:
-            graph[w]=[]
-            PARAMS = {
-                "action": "query",
-                "format": "json",
-                "titles": w,
-                "prop": "links"
-            }
-
-            R = S.get(url=URL, params=PARAMS)
-            DATA = R.json()
-
-            PAGES = DATA["query"]["pages"]
-            for k, v in PAGES.items():
-                if "links" in v:
-
-                    for l in v["links"]:
-                        if l["title"] in relate or l["title"] in graph:
-                            count=count+1
-
-                        graph[w].append(l["title"])
-                        relate.add(l["title"])
-                        if count>=node_number:
-                            print(graph)
-                            return graph
-
-    print(graph)
-    return graph
-
-
-# Create the main window
-window = tk.Tk()
-window.title("Distributed PageRank")
-window.columnconfigure([0, 1, 2], minsize=150)
-window.rowconfigure([0, 1, 2, 3], minsize=50)
-
-# Create a label asking for the title
-titleLabel = tk.Label(text="Enter the title of the page: ")
-titleLabel.grid(row=0, column=0)
- 
-titleEntry = tk.Entry()
-titleEntry.grid(row=0, column=1, sticky='WE', padx=15, columnspan=2)
-
-# Create a label asking for the number of nodes
-numNodesLabel = tk.Label(text="Enter the number of nodes: ")
-numNodesLabel.grid(row=1, column=0)
-
-numNodesEntry = tk.Entry()
-numNodesEntry.grid(row=1, column=1, sticky='WE', padx=15, columnspan=2)
-
-# Create a button to start the program
-button = tk.Button(text="Start", command=print_graph)
-button.grid(row=2, column=1)
-
-# Create a text box to display the output
-textframe = tk.Frame(master=window)
-textframe.grid(row=3, column=0, sticky="WE", columnspan=3)
-text = tk.Text(master=textframe, wrap=tk.WORD)
-text.pack()
-text.insert(tk.END, 'Results will show up here')
-
-window.mainloop()
+if __name__ == "__main__":
+    main()
